7facc7db877b089b6ebfc3a32ef42e9374715373
Sonu Agrawal <sonuagrawal3323@gmail.com>
2024-05-14 00:39:34 +0530

    Prime Number

---
+++ InterviewPrep/src/com/interview/PrimeNumber.java
@@ -0,0 +1,37 @@
+package com.interview;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number : ");
+		
+		int num = sc.nextInt();
+		boolean isPrime = checkIsPrime(num);
+		if(isPrime)
+			System.out.println("Prime Number");
+		else 
+			System.out.println("Not Prime");
+	}
+	
+	public static boolean checkIsPrime(int num) {
+		boolean isPrime = false;
+		if(num == 2) {
+			isPrime = true;
+			return isPrime;
+		}
+			
+		for(int i = 3; i < num; i++) {
+			if(num%i == 0) {
+				isPrime = false;
+				break;
+			}
+			isPrime = true;
+		}
+		return isPrime;
+	}
+
+}
